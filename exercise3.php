58cbd438eb2ac278e503d5631947129b00d7ddd5
Allen Jefferson <94884406+0rcino@users.noreply.github.com>
2024-09-06 16:20:30 +0800

    new fix

--- exercise3.php
+++ exercise3.php
@@ -1,26 +1,23 @@
 <?php
-// Define the file name
+
 $filename = 'allen.txt';
 
-// Check if the file exists
 if (file_exists($filename)) {
-    // Read the contents of the file
+   
     $contents = file_get_contents($filename);
-    echo "File contents:\n" . $contents . "\n";
+    echo "contents of file:\n" . $contents . "\n";
 } else {
-    echo "File does not exist. Creating the file...\n";
-    // Create the file and write some initial content
+    echo "There is no file. Making the file...\n";
+   
     $initialContent = "Hello i'm Allen \nthis is Group 2\nThis is a sample file.";
     file_put_contents($filename, $initialContent);
-    echo "File created and initial content written.\n";
+    echo "First material was written and the file was produced..\n";
 }
 
-// Append new content to the file
-$newContent = "\nAdding a new line to the file.";
+$newContent = "\nentering a new line in the file.";
 file_put_contents($filename, $newContent, FILE_APPEND);
 
-// Read the file into an array, each line as an element
 $fileArray = file($filename);
-echo "File contents as an array:\n";
+echo "contents of the file as an array:\n";
 print_r($fileArray);
 ?>
